bf57b33eb9f35903d6a9074d73047688fca1fef1
Luiz Parente <luiz.par@outlook.com>
2021-10-22 22:31:28 -0400

    Conditional Statements.

---
+++ IfStatements/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace IfStatements {
+	class Program {
+		static void Main(string[] args) {
+			// This is where the magic happens.
+
+			// IF STATEMENTS
+			// If statements are control structures that
+			// define whether a code block will run.
+
+			// If statements have two parts:
+			// 1. Declaration (header).
+			// 2. Body.
+			// 3. Sometimes, if statements will have "else if" statements.
+			// 4. Sometimes, if statements will have "else" statements.
+
+			// Boolean statements are statements that return true or false.
+
+			// Ex.: It is raining outside.
+			// > false.
+
+			// Ex.: The lights are on.
+			// > true.
+
+			// Ex.: 3 > 5
+			// > false.
+
+			// Ex.: 10 >= 5
+			// > true.
+
+			// Boolean statements can use comparison operators.
+			// ==
+			// !=
+			// >
+			// >=
+			// <
+			// <=
+
+			// We are working on a program that outputs different messages
+			// depending on the city provided by the user.
+
+			string city = "Toronto";
+
+			if (city == "Toronto") {
+				// This code will run if city is equal to Toronto.
+				Console.WriteLine("Toronto is a city in Canada.");
+			}
+			else if (city == "New York") {
+				// This will only run if the first scenario is false.
+				Console.WriteLine("New York is a city in the US.");
+			}
+			else if (city == "Paris") {
+				// This will only run if the first and second scenarios are false.
+				Console.WriteLine("Paris is a city in France.");
+			}
+			else {
+				// This will only run if all of the scenarios above are false.
+				Console.WriteLine("I do not know that city.");
+			}
+
+			// Code continues...
+		}
+	}
+}
---
+++ SwitchStatements/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SwitchStatements {
+	class Program {
+		static void Main(string[] args) {
+			// Here is where the magic happens.
+
+			// SWITCH STATEMENTS
+			// Switch statements are conditional structures that define
+			// whether a code block will run.
+
+			// We are working a program that outputs different messages
+			// depending on the city provided by the user.
+
+			string city = "Toronto";
+
+			// Switch statements have two parts:
+			// 1. Declaration (header).
+			// 2. Cases.
+
+			switch (city) {
+				case "Toronto":
+					Console.WriteLine("Toronto is a city in Canada.");
+					break;
+				case "New York":
+					Console.WriteLine("New York is a city in the US.");
+					break;
+				case "Paris":
+					Console.WriteLine("Paris is a city in France.");
+					break;
+
+				// Optionally, a default case can be defined.
+				// It will run for all possible scenarios not defined above.
+				default:
+					Console.WriteLine("I do not know that city.");
+					break;
+			}
+
+			// Code continues...
+		}
+	}
+}
